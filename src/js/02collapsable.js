6e670b5911a5be17e8fdff6a0c6da19851c4bdd9
Ana Montegrifo <amontegrifo@gmail.com>
2021-09-06 14:27:38 +0200

    add collapsable-section share

--- src/js/02collapsable.js
+++ src/js/02collapsable.js
@@ -5,14 +5,13 @@
 //const arrowButton = document.querySelector('.js-arrow');
 const chevronUp = document.querySelector('.js-chevron');
 const collapsableSection = document.querySelector('.js-collapsable');
-const container = document.querySelector('.container');
 
 function foldAndUnfold(event) {
-  event.preventDefault();
-  console.log(event.target);
-  console.log(event.currentTarget);
-  collapsableSection.classList.toggle('hidden');
-  chevronUp.classList.toggle('upDown');
+	event.preventDefault();
+	console.log(event.target);
+	console.log(event.currentTarget);
+	collapsableSection.classList.toggle('hidden');
+	chevronUp.classList.toggle('upDown');
 }
 
 /*function changeArrow() {
@@ -45,12 +44,25 @@ chevronUp.addEventListener('click', foldAndUnfold);
 //const arrowButton2 = document.querySelector('.js-arrow2');
 const chevronUp2 = document.querySelector('.js-chevron2');
 const collapsableSection2 = document.querySelector('.js-collapsable2');
-const container2 = document.querySelector('.container2');
 
 function foldAndUnfold2(event) {
-  event.preventDefault();
-  collapsableSection2.classList.toggle('hidden');
-  chevronUp2.classList.toggle('upDown');
+	event.preventDefault();
+	collapsableSection2.classList.toggle('hidden');
+	chevronUp2.classList.toggle('upDown');
 }
 
 chevronUp2.addEventListener('click', foldAndUnfold2);
+
+// SHARE SECTION
+
+//const arrowButton2 = document.querySelector('.js-arrow2');
+const chevronUp3 = document.querySelector('.js-chevron3');
+const collapsableSection3 = document.querySelector('.js-collapsable3');
+
+function foldAndUnfold3(event) {
+	event.preventDefault();
+	collapsableSection3.classList.toggle('hidden');
+	chevronUp3.classList.toggle('upDown');
+}
+
+chevronUp3.addEventListener('click', foldAndUnfold3);
