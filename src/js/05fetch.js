5afcb9159c46282f9fab6fea3f67268f87a474fb
Ana Montegrifo <amontegrifo@gmail.com>
2021-09-14 17:39:42 +0200

    hide by default the twitter button

--- src/js/05fetch.js
+++ src/js/05fetch.js
@@ -13,24 +13,20 @@ let data = {
 const form = document.querySelector('.js-form');
 
 function handlerFormData(e) {
-	console.log(e.target.name);
-	console.log(e.currentTarget);
-
 	const inputId = e.target.name;
 	const inputValue = e.target.value;
 
 	data[inputId] = inputValue;
-	console.log(data);
 }
 
 form.addEventListener('change', handlerFormData);
 
 const createBtn = document.querySelector('.js-createBtn');
 const cardResultElement = document.querySelector('.js-url');
+const cardCreated = document.querySelector('.js-cardCreated');
 
 function handleCreateBtn(event) {
 	event.preventDefault();
-	console.log(data);
 
 	fetch('https://awesome-profile-cards.herokuapp.com/card', {
 		method: 'POST',
@@ -41,11 +37,12 @@ function handleCreateBtn(event) {
 	})
 		.then((response) => response.json())
 		.then((data) => {
-			console.log(data);
 			cardResultElement.innerHTML = data.cardURL;
 			cardResultElement.href = data.cardURL;
-			console.log(cardResultElement);
-			console.log(data.cardURL);
+
+			if (data.success) {
+				cardCreated.classList.remove('hidden');
+			}
 		});
 }
 
